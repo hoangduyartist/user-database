e90d16ed98a581687cf8d27c2c8c53c6ad7c24c8
hoangduyartist <hoangduy.artist@gmail.com>
2019-06-14 09:36:56 +0700

    check account is verified and send text-email

--- services/userService.js
+++ services/userService.js
@@ -16,7 +16,6 @@ module.exports = {
 async function create(userParams) {
 
     // validate
-
     const userValidate = await User.findOne({ username: userParams.username });
 
     if (userValidate) {
@@ -47,11 +46,11 @@ async function create(userParams) {
         });
 
         var mailOptions = {
-            from: 'nguyenhoangduy19598@gmail.com',
-            to: 'hoangduy.artist@gmail.com, hotuanvu1234@gmail.com, thienngan12796@gmail.com',
-            subject: 'Sending Email using Node.js',
-            text: `nodejs-send-email.`
-            // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+            from: 'userservicetraining2019@gmail.com',
+            to: newuser.email,
+            subject: 'Global Traning - Verify email',
+            text: `Hi there, please verify email to active your account. Click link below`
+            //html: `<h3 style="font-family:segoe ui light"><a href="http://localhost:3000/web-api/new-user/verify-email.${newuser._id}"></a></h3>`        
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
@@ -62,7 +61,7 @@ async function create(userParams) {
             }
         });
         //end-send-email
-        return ({ status: "success", newuser: newuser, message: "Register successful!" })
+        return ({ status: "success", newuser: newuser, message: "Register successful!", todo: `Email sent to ${newuser.email}. Check email to acive your account.` })
     }
 
     return ({ status: "error", message: "Register failed" })
@@ -75,6 +74,9 @@ async function authenticate({ username, password }) {
         return ({ status: "error", message: 'user-name is incorrect!' })
 
     if (user && bcrypt.compareSync(password, user.password)) {
+        if(user.isVerified == false )
+        return {status: "error", message: "your account hasn't been activated."}
+
         const token = jwt.sign({ id: user._id }, 'secret12345', { expiresIn: '1h' });
         // LocalStorage.setItem("token", JSON.stringify(token));
         return { status: "success", message: "user found!", data: { user: user, token: token } }
